0e57ab72d15337390a2cd11bbbe191e6eb119728
yi985432 <70029274+yi985432@users.noreply.github.com>
2021-03-12 10:32:06 -0600

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/sh
+#网站删除检测脚本 by rm -rf /*[3586563103]
+#************************************************
+#在此输入你虚拟主机的ftp地址和用户名密码
+ftp="23.224.53.118"
+name="s4947674"
+password="ztAM99mhIQ"
+#**************************************************
+if [ ! -f ./*.web.7z ];then
+echo "没有源码！"
+exit
+else
+.
+fi
+apt install curlftpfs
+mkdir work
+curlftpfs ftp://$name:$password@$ftp work
+cd work
+if [ ! -d ./wwwroot/usr ];then
+echo "未检测到usr文件夹！正在恢复！"
+rm -rf wwwroot
+7z x ../*.web.7z -d .
+else
+echo "已检测到usr文件夹，不进行任何操作。"
+fi
+#ftp -n $ftp << EOF 
+#passive
+#user $name $password
+#cd /wwwroot
+#ls
+#bye
+#EOF
